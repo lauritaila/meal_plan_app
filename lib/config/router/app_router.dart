4d48231988faa8a657f60bbc185a1cd1e48cb5fb
Laura Avila <lauritaila10gmail.com>
2025-06-23 20:04:07 -0400

    FEAT: added login screen to router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:meal_plan_app/features/features.dart';
 
-final appRouter = GoRouter(routes: [
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
   GoRoute(path: '/', builder: (context, state) => const InitScreen()),
+  GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
 ]);
