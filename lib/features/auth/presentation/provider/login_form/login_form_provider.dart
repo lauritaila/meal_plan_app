af510a19eccb624569e5a3041c1b4c270702eaae
Laura Avila <lauritaila10gmail.com>
2025-06-30 20:36:43 -0400

    FEAT: login form provider

---
+++ lib/features/auth/presentation/provider/login_form/login_form_provider.dart
@@ -0,0 +1,112 @@
+import 'package:formz/formz.dart';
+import 'package:meal_plan_app/features/auth/presentation/provider/repository/repository_provider.dart';
+import 'package:meal_plan_app/features/shared/shared.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart'; 
+
+part 'login_form_provider.g.dart';
+
+
+class LoginFormState {
+  final bool isPosting;
+  final bool isFormPosted;
+  final bool isValid;
+  final Email email;
+  final Password password;
+
+  LoginFormState({
+    this.isPosting = false,
+    this.isFormPosted = false,
+    this.isValid = false,
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+  });
+
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) =>
+      LoginFormState(
+        isPosting: isPosting ?? this.isPosting,
+        isFormPosted: isFormPosted ?? this.isFormPosted,
+        isValid: isValid ?? this.isValid,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  @override
+  String toString() {
+    return '''
+LoginFormState:
+  isPosting: $isPosting
+  isFormPosted: $isFormPosted
+  isValid: $isValid
+  email: $email
+  password: $password
+''';
+  }
+}
+
+
+@riverpod
+class LoginForm extends _$LoginForm { 
+  @override
+  LoginFormState build() {
+    return LoginFormState();
+  }
+
+  void onEmailChanged(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  void onPasswordChanged(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([newPassword, state.email]),
+    );
+  }
+
+  Future<void> onFormSubmitted() async {
+    _touchEveryField(); 
+
+    if (!state.isValid) {
+      // Opcional: mostrar un mensaje de error si el formulario no es válido
+      // ref.read(authProvider.notifier).showSnackbar('Por favor, corrige los errores del formulario.');
+      return;
+    }
+
+    state = state.copyWith(isPosting: true);
+
+    try {
+      await ref.read(authRepositoryProvider).logIn(
+        state.email.value,
+        state.password.value,
+      );
+
+    } catch (e) {
+      print(e);
+    } finally {
+      state = state.copyWith(isPosting: false);
+    }
+  }
+
+  // Método auxiliar para "tocar" cada campo (marcarlo como modificado)
+  void _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true, // Indica que el formulario ha sido intentado enviar
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/provider/login_form/login_form_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_form_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginFormHash() => r'6794633e64d896d28cf18457566cbd9c04b714f4';
+
+/// See also [LoginForm].
+@ProviderFor(LoginForm)
+final loginFormProvider =
+    AutoDisposeNotifierProvider<LoginForm, LoginFormState>.internal(
+      LoginForm.new,
+      name: r'loginFormProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$loginFormHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$LoginForm = AutoDisposeNotifier<LoginFormState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
