6d59bfc2ee8c532bca828c7009768f561c1c12ea
Laura Avila <lauritaila10gmail.com>
2025-06-30 20:40:01 -0400

    FEAT: router provider to manage protected routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,10 +1,47 @@
-
 import 'package:go_router/go_router.dart';
-import 'package:meal_plan_app/features/features.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // You need Riverpod to listen
+import 'package:meal_plan_app/features/auth/presentation/provider/provider.dart';
+import 'package:meal_plan_app/features/auth/auth.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-  GoRoute(path: '/', builder: (context, state) => const InitScreen()),
-  GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
-]);
+import '../../features/shared/shared.dart';
+
+final appRouterProvider = Provider((ref) {
+  final authState = ref.watch(authProvider);
+
+  return GoRouter(
+    initialLocation: '/init',
+    routes: [
+      GoRoute(path: '/init', builder: (context, state) => const InitScreen()),
+      GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+      // GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+    ],
+    redirect: (context, state) {
+      final isGoingToLogin = state.fullPath == '/login';
+      final isGoingToInit = state.fullPath == '/init';
+
+      // 1. If the state is loading or initial
+      if (authState is InitialAuthState || authState is LoadingAuthState) { 
+        return isGoingToInit ? null : '/init';
+      }
+
+      // 2. If the user is NOT authenticated or there is an authentication error
+      if (authState is UnauthenticatedAuthState || authState is ErrorAuthState) { 
+        return isGoingToLogin ? null : '/login';
+      }
+
+      // 3. If the user IS authenticated
+      if (authState is AuthenticatedAuthState) { 
+        // return (isGoingToLogin || isGoingToInit) ? '/home' : null;
+      }
+
+      // 4. For the message state (temporary)
+      if (authState is MessageAuthState) { 
+        // We do not redirect based on a message, let the normal flow continue
+        return null;
+      }
+
+      return null;
+    },
+    errorBuilder: (context, state) => ErrorScreen(error: state.error),
+  );
+});
