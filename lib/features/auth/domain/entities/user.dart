b34c5ea1c7e2228843063ef146b9bdf7e60533e3
Laura Avila <lavila@bigwise.com>
2025-07-03 15:14:11 -0400

    REFAC: added copyWith and Equatable to UserProfile

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,15 +1,33 @@
 
-class UserProfile {
-  final String id; // El ID del usuario de Supabase Auth
-  final String email;
-  final String? name; // Puede ser nulo
-  final Map<String, dynamic>? profileData; // jsonb en la BD
+import 'package:equatable/equatable.dart'; 
 
-  UserProfile({
+class UserProfile extends Equatable {
+  final String id;
+  final String email;
+  final String? name;
+  final Map<String, dynamic>? profileData;
+
+  const UserProfile({
     required this.id,
     required this.email,
     this.name,
     this.profileData,
   });
 
+  UserProfile copyWith({
+    String? id,
+    String? email,
+    String? name,
+    Map<String, dynamic>? profileData,
+  }) {
+    return UserProfile(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      profileData: profileData ?? this.profileData,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, profileData];
 }
